8d36dd6f45e2bcc04d3ba0e66bee70b3271b7e59
daniel-carlos <31622540+daniel-carlos@users.noreply.github.com>
2017-12-05 00:36:18 -0300

    GameOver

--- Assets/Objetos/GameControllers/Estados/FSM_GC_GameOver.cs
+++ Assets/Objetos/GameControllers/Estados/FSM_GC_GameOver.cs
@@ -3,9 +3,12 @@ using System.Collections;
 
 public class FSM_GC_GameOver : GC_Estado
 {
+	public GameObject menuGO;
+
 	public override void Entrar (FSM fsm)
 	{
-		
+		menuGO.SetActive (true);
+		//menuGO.GetComponent<Animator>()
 	}
 
 	public override void Executar (FSM fsm)
